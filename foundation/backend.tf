a7e49f1ede0790fc76ad35d3b06eb1595500815c
matanweisz <matan.weisz@infinity.co.il>
2025-11-25 14:03:58 +0200

    backend fix

--- foundation/backend.tf
+++ foundation/backend.tf
@@ -1,7 +1,9 @@
 terraform {
   backend "s3" {
     bucket       = "foundation-terraform-project-state"
+    key          = "terraform.tfstate"
     region       = "eu-central-1"
     use_lockfile = true
+    encrypt      = true
   }
 }
